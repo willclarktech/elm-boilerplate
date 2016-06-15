26d235ed8661dd80a0889539a1f41f8668a4211e
LikeJasper <williamclark.home@gmail.com>
2016-06-15 16:41:07 +0200

    :vertical_traffic_light: elm: add basic elm test setup

---
+++ elm-test/TestRunner.elm
@@ -0,0 +1,6 @@
+import ElmTest exposing (runSuite)
+import Tests exposing (testSuite)
+
+main : Program Never
+main =
+  runSuite testSuite
---
+++ elm-test/Tests.elm
@@ -0,0 +1,8 @@
+module Tests exposing (testSuite)
+
+import ElmTest exposing (Test, suite)
+
+testSuite : Test
+testSuite =
+  suite "All tests"
+    []
