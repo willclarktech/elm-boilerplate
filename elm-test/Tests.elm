2794f8604d33fa167568454e7869666f291ce2f3
LikeJasper <williamclark.home@gmail.com>
2016-06-15 17:03:57 +0200

    :vertical_traffic_light: elm: add sample elm tests

--- elm-test/Tests.elm
+++ elm-test/Tests.elm
@@ -1,8 +1,10 @@
 module Tests exposing (testSuite)
 
 import ElmTest exposing (Test, suite)
+import App.SampleTest
 
 testSuite : Test
 testSuite =
   suite "All tests"
-    []
+    [ App.SampleTest.testSuite
+    ]
