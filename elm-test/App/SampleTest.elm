fa3ee6cdaaf8996867588fd86ac11d1aa42f4345
LikeJasper <williamclark.home@gmail.com>
2016-06-19 00:24:51 +0200

    :scissors: sample-test: use bdd style

--- elm-test/App/SampleTest.elm
+++ elm-test/App/SampleTest.elm
@@ -1,32 +1,37 @@
 module App.SampleTest exposing (testSuite)
 
 import App.Sample exposing (sum, product)
-import ElmTest exposing (Test, suite, test, assertEqual)
-import Check exposing (quickCheck, claim, true, that, is, for)
+import ElmTestBDDStyle exposing (Test, describe, it, expect, toBe, itAlways, expectThat, isTheSameAs, forEvery)
 import Check.Producer exposing (filter, tuple, int, float)
-import Check.Test exposing (evidenceToTest)
 
 
 testSuite : Test
 testSuite =
-    suite "App.Sample"
-        [ suite "sum"
-            [ test "should return a sum of 2 Ints" <| assertEqual 8 (sum 3 5)
-            , test "should return a sum of 2 Floats" <| assertEqual -10.5 (sum -20.5 10)
+    describe "App.Sample"
+        [ describe "sum"
+            [ it "should return a sum of 2 Ints"
+                <| expect 8 toBe (sum 3 5)
+            , it "should return a sum of 2 Floats"
+                <| expect -10.5 toBe (sum -20.5 10)
             ]
-        , evidenceToTest
-            << quickCheck
-            <| Check.suite "product"
-                [ claim "should multiply Ints"
-                    `that` (\( a, b ) -> product a b)
-                    `is` (\( a, b ) -> a * b)
-                    `for` tuple ( int, int )
-                , claim "should multiply Floats"
-                    `that` (\( a, b ) -> product a b)
-                    `is` (\( a, b ) -> a * b)
-                    `for` tuple ( float, float )
-                , claim "should be inverted by division with minimal imprecision"
-                    `true` (\( a, b ) -> abs (product a b / b - a) < 1.0e-6)
-                    `for` (filter (\( a, b ) -> b /= 0) (tuple ( float, float )))
-                ]
+        , describe "product"
+            [ itAlways "multiplies Ints"
+                <| expectThat (\( a, b ) -> product a b)
+                    isTheSameAs
+                    (\( a, b ) -> a * b)
+                    forEvery
+                    (tuple ( int, int ))
+            , itAlways "multiplies Floats"
+                <| expectThat (\( a, b ) -> product a b)
+                    isTheSameAs
+                    (\( a, b ) -> a * b)
+                    forEvery
+                    (tuple ( float, float ))
+            , itAlways "inverts by division with minimal imprecision"
+                <| expectThat (\( a, b ) -> abs (product a b / b - a) < 1.0e-6)
+                    isTheSameAs
+                    (\( a, b ) -> True)
+                    forEvery
+                    (filter (\( a, b ) -> b /= 0) (tuple ( float, float )))
+            ]
         ]
