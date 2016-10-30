32dc54aa04f5f3c6bf272249a436b956c0cd060c
willclarktech <williamclark.home@gmail.com>
2016-10-30 16:34:22 +0100

    :seedling: mdl: move main view into separate file

--- src/Todos/View/Index.elm
+++ src/Todos/View/Index.elm
@@ -1,7 +1,6 @@
 module Todos.View.Index exposing (view)
 
 import Html exposing (..)
-import Html.Attributes exposing (..)
 import Material.Layout as Layout
 import Todos.Types
     exposing
@@ -12,11 +11,8 @@ import Todos.Types
         , FilterOption(..)
         )
 import Todos.View.Heading exposing (viewHeading)
-import Todos.View.Info exposing (viewInfo)
-import Todos.View.NewTodoInput exposing (viewNewTodoInput)
-import Todos.View.Filters exposing (viewFilters)
 import Todos.View.Tabs exposing (viewTabs)
-import Todos.View.Todos exposing (viewTodos)
+import Todos.View.Main exposing (viewMain)
 
 
 view : Model -> Html Msg
@@ -29,9 +25,9 @@ view model =
         , Layout.onSelectTab SwitchTab
         ]
         { header = [ viewHeading model ]
+        , tabs = ( viewTabs, [] )
         , drawer = []
         , main = [ viewMain model ]
-        , tabs = ( viewTabs, [] )
         }
 
 
@@ -43,56 +39,3 @@ getSelectedTab tab =
 
         Info ->
             1
-
-
-viewMain : Model -> Html Msg
-viewMain { tab, currentText, todos, filterOption, currentlyEditing, oauth, mdl } =
-    let
-        relevantTodos =
-            getTodosForFilterOption todos filterOption
-
-        baseComponents =
-            case tab of
-                Todos ->
-                    [ viewNewTodoInput mdl currentText
-                    ]
-
-                Info ->
-                    [ viewInfo
-                    ]
-
-        showSaveButton =
-            case oauth.userName of
-                Nothing ->
-                    False
-
-                Just _ ->
-                    True
-
-        components =
-            if tab == Todos && List.length todos /= 0 then
-                List.append baseComponents
-                    [ viewFilters filterOption showSaveButton
-                    , viewTodos relevantTodos currentlyEditing
-                    ]
-            else
-                baseComponents
-    in
-        div
-            [ id "todos-app"
-            , class "ui segments container"
-            ]
-            components
-
-
-getTodosForFilterOption : List Todo -> FilterOption -> List Todo
-getTodosForFilterOption todos filterOption =
-    case filterOption of
-        Completed ->
-            List.filter (\todo -> todo.completed) todos
-
-        Incomplete ->
-            List.filter (\todo -> todo.completed /= True) todos
-
-        All ->
-            todos
---
+++ src/Todos/View/Main.elm
@@ -0,0 +1,62 @@
+module Todos.View.Main exposing (viewMain)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Todos.Types exposing (Model, Msg, Todo, Tab(..), FilterOption(..))
+import Todos.View.NewTodoInput exposing (viewNewTodoInput)
+import Todos.View.Info exposing (viewInfo)
+import Todos.View.Filters exposing (viewFilters)
+import Todos.View.Todos exposing (viewTodos)
+
+
+viewMain : Model -> Html Msg
+viewMain { tab, currentText, todos, filterOption, currentlyEditing, oauth, mdl } =
+    let
+        relevantTodos =
+            getTodosForFilterOption todos filterOption
+
+        baseComponents =
+            case tab of
+                Todos ->
+                    [ viewNewTodoInput mdl currentText
+                    ]
+
+                Info ->
+                    [ viewInfo
+                    ]
+
+        showSaveButton =
+            case oauth.userName of
+                Nothing ->
+                    False
+
+                Just _ ->
+                    True
+
+        components =
+            if tab == Todos && List.length todos /= 0 then
+                List.append baseComponents
+                    [ viewFilters filterOption showSaveButton
+                    , viewTodos relevantTodos currentlyEditing
+                    ]
+            else
+                baseComponents
+    in
+        div
+            [ id "todos-app"
+            , class "ui segments container"
+            ]
+            components
+
+
+getTodosForFilterOption : List Todo -> FilterOption -> List Todo
+getTodosForFilterOption todos filterOption =
+    case filterOption of
+        Completed ->
+            List.filter (\todo -> todo.completed) todos
+
+        Incomplete ->
+            List.filter (\todo -> todo.completed /= True) todos
+
+        All ->
+            todos
